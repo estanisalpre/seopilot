1184e2d7ae3fac7f726157ce0e7ef7601aa1ce06
esalinas-agricapital <esalinas@agricapital.com.co>
2025-07-14 10:39:32 -0500

    refactor: simplify imports and improve code readability in index.js, reporter.js, runner.js, and detectFramework.js

--- dist/utils/detectFramework.js
+++ dist/utils/detectFramework.js
@@ -1,26 +1,20 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.detectOutputDir = detectOutputDir;
-const fs_1 = __importDefault(require("fs"));
-function detectOutputDir() {
+import fs from 'fs';
+export function detectOutputDir() {
     const cwd = process.cwd();
-    if (fs_1.default.existsSync(`${cwd}/astro.config.mjs`))
+    if (fs.existsSync(`${cwd}/astro.config.mjs`))
         return 'dist';
-    if (fs_1.default.existsSync(`${cwd}/next.config.js`)) {
-        if (fs_1.default.existsSync(`${cwd}/out`))
+    if (fs.existsSync(`${cwd}/next.config.js`)) {
+        if (fs.existsSync(`${cwd}/out`))
             return 'out'; // SSG export
         return '.next'; // SSR - podríamos advertir que no es soportado aún
     }
-    if (fs_1.default.existsSync(`${cwd}/vue.config.js`) || fs_1.default.existsSync(`${cwd}/vite.config.ts`)) {
-        if (fs_1.default.existsSync(`${cwd}/dist`))
+    if (fs.existsSync(`${cwd}/vue.config.js`) || fs.existsSync(`${cwd}/vite.config.ts`)) {
+        if (fs.existsSync(`${cwd}/dist`))
             return 'dist';
     }
-    if (fs_1.default.existsSync(`${cwd}/nuxt.config.js`) || fs_1.default.existsSync(`${cwd}/.output/public`))
+    if (fs.existsSync(`${cwd}/nuxt.config.js`) || fs.existsSync(`${cwd}/.output/public`))
         return '.output/public';
-    if (fs_1.default.existsSync(`${cwd}/build`))
+    if (fs.existsSync(`${cwd}/build`))
         return 'build'; // CRA
     // Fallback
     return 'dist';
